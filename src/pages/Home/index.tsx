0e661fbc96d47aa28575884db9ace1a3c029092a
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2024-10-09 05:20:36 +0100

    set up fonts and routes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='px-10 py-20 flex flex-col gap-5'>
-      <Link to="/">Home</Link>
-      <Link to="records">NewsRecord</Link>
-      <Link to="about">About</Link>
+    <div className='flex align-center justify-between'>
+      <p className='text-lg'><span className='font-semibold'>Bloc</span>Health</p>
+      <div className='flex align-center gap-4'>
+        <Link to="/">Home</Link>
+        <Link to="records">NewsRecord</Link>
+        <Link to="about">About</Link>
+      </div>
+      <Button>Connect Wallet</Button>
     </div>
   )
 }
